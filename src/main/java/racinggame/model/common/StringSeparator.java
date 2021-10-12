422f84d36e3234387298d054a23760823258f5c0
devyonghee <devyonghee@gmail.com>
2021-10-12 22:57:46 +0900

    feat (StringSeparator) : 문자 분할 기능 추가
    - , 으로 주어지는 이름을 구분해주는 객체 추가

--- src/main/java/racinggame/model/common/StringSeparator.java
+++ src/main/java/racinggame/model/common/StringSeparator.java
@@ -4,18 +4,36 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 
-public class StringSeparator implements StringsProvider {
+public final class StringSeparator implements StringsProvider {
 
 	private final String target;
 	private final String delimiter;
 
-	public StringSeparator(String target, String delimiter) {
+	private StringSeparator(String target, String delimiter) {
+		validateTarget(target);
+		validateDelimiter(delimiter);
 		this.target = target;
 		this.delimiter = delimiter;
+	}
+
+	public static StringSeparator of(String target, String delimiter) {
+		return new StringSeparator(target, delimiter);
 	}
 
 	@Override
 	public Collection<String> provide() {
 		return Collections.unmodifiableList(Arrays.asList(target.split(this.delimiter)));
+	}
+
+	private void validateDelimiter(String delimiter) {
+		if (delimiter == null) {
+			throw new IllegalArgumentException("delimiter must not be null");
+		}
+	}
+
+	private void validateTarget(String target) {
+		if (target == null || "".equals(target.trim())) {
+			throw new IllegalArgumentException("string to be split must not be empty");
+		}
 	}
 }
---
+++ src/test/java/racinggame/model/common/StringSeparatorTest.java
@@ -0,0 +1,40 @@
+package racinggame.model.common;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class StringSeparatorTest {
+
+	@Test
+	@DisplayName("객체화")
+	void instance() {
+		assertThatNoException()
+			.isThrownBy(() -> StringSeparator.of("string", ","));
+	}
+
+	@Test
+	@DisplayName("분할할 문자가 없는 객체화")
+	void instance_emptyString_illegalArgumentExceptionThrown() {
+		assertThatIllegalArgumentException()
+			.isThrownBy(() -> StringSeparator.of("", ","))
+			.withMessageContaining("string to be split must not be empty");
+	}
+
+	@Test
+	@DisplayName("구분자가 없는 객체화")
+	void instance_nullDelimiter_illegalArgumentExceptionThrown() {
+		assertThatIllegalArgumentException()
+			.isThrownBy(() -> StringSeparator.of("string", null))
+			.withMessageContaining("delimiter must not be null");
+	}
+
+	@Test
+	@DisplayName("분할된 문자 모음")
+	void provide() {
+		assertThat(StringSeparator.of("string", "").provide())
+			.hasSize(6)
+			.containsExactly("s", "t", "r", "i", "n", "g");
+	}
+}
