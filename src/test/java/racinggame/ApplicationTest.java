754776cddabe28f44b911ad307c41fcbfe701ffb
devyonghee <devyonghee@gmail.com>
2021-10-13 14:27:11 +0900

    style(utils, test) : 파일 formatting
    - 기존 파일 포맷 스타일 변경

--- src/test/java/racinggame/ApplicationTest.java
+++ src/test/java/racinggame/ApplicationTest.java
@@ -13,69 +13,69 @@ import org.junit.jupiter.api.function.Executable;
 import nextstep.test.NSTest;
 
 public class ApplicationTest extends NSTest {
-    private static final int MOVING_FORWARD = 4;
-    private static final int STOP = 3;
+	private static final int MOVING_FORWARD = 4;
+	private static final int STOP = 3;
 
-    private static final String ERROR_MESSAGE = "[ERROR]";
+	private static final String ERROR_MESSAGE = "[ERROR]";
 
-    @BeforeEach
-    void beforeEach() {
-        setUp();
-    }
+	@Override
+	public void runMain() {
+		Application.main(new String[] {});
+	}
 
-    // @Test
-    // void 전진_정지() {
-    //     assertRandomTest(() -> {
-    //         run("pobi,woni", "1");
-    //         verify("pobi : -", "woni : ", "최종 우승자는 pobi 입니다.");
-    //     }, MOVING_FORWARD, STOP);
-    // }
+	// @Test
+	// void 전진_정지() {
+	//     assertRandomTest(() -> {
+	//         run("pobi,woni", "1");
+	//         verify("pobi : -", "woni : ", "최종 우승자는 pobi 입니다.");
+	//     }, MOVING_FORWARD, STOP);
+	// }
 
-    // @Test
-    // void 이름에_대한_예외_처리() {
-    //     assertSimpleTest(() -> {
-    //         runNoLineFound("pobi,javaji");
-    //         verify(ERROR_MESSAGE);
-    //     });
-    // }
+	// @Test
+	// void 이름에_대한_예외_처리() {
+	//     assertSimpleTest(() -> {
+	//         runNoLineFound("pobi,javaji");
+	//         verify(ERROR_MESSAGE);
+	//     });
+	// }
 
-    @Test
-    void 전진_정지() {
-        assertRandomTestAtMostOneSecond(() -> {
-            run("pobi,woni", "1");
-            verify("pobi : -", "woni : ", "최종 우승자는 pobi 입니다.");
-        }, MOVING_FORWARD, STOP);
-    }
+	@BeforeEach
+	void beforeEach() {
+		setUp();
+	}
 
-    @Test
-    void 이름에_대한_예외_처리() {
-        assertSimpleTestAtMostOneSecond(() -> {
-            runNoLineFound("pobi,javaji");
-            verify(ERROR_MESSAGE);
-        });
-    }
+	@Test
+	void 전진_정지() {
+		assertRandomTestAtMostOneSecond(() -> {
+			run("pobi,woni", "1");
+			verify("pobi : -", "woni : ", "최종 우승자는 pobi 입니다.");
+		}, MOVING_FORWARD, STOP);
+	}
 
-    private void assertRandomTestAtMostOneSecond(final Executable executable, final int value, final int... values) {
-        assertAtMostOneSecond(() -> assertRandomTest(executable, value, values));
-    }
+	@Test
+	void 이름에_대한_예외_처리() {
+		assertSimpleTestAtMostOneSecond(() -> {
+			runNoLineFound("pobi,javaji");
+			verify(ERROR_MESSAGE);
+		});
+	}
 
-    private void assertSimpleTestAtMostOneSecond(Executable executable) {
-        assertAtMostOneSecond(() -> assertSimpleTest(executable));
-    }
+	@AfterEach
+	void tearDown() {
+		outputStandard();
+	}
 
-    private void assertAtMostOneSecond(ThrowingRunnable runnable) {
-        await()
-            .atMost(Duration.ofSeconds(1))
-            .untilAsserted(runnable);
-    }
+	private void assertRandomTestAtMostOneSecond(final Executable executable, final int value, final int... values) {
+		assertAtMostOneSecond(() -> assertRandomTest(executable, value, values));
+	}
 
-    @AfterEach
-    void tearDown() {
-        outputStandard();
-    }
+	private void assertSimpleTestAtMostOneSecond(Executable executable) {
+		assertAtMostOneSecond(() -> assertSimpleTest(executable));
+	}
 
-    @Override
-    public void runMain() {
-        Application.main(new String[] {});
-    }
+	private void assertAtMostOneSecond(ThrowingRunnable runnable) {
+		await()
+			.atMost(Duration.ofSeconds(1))
+			.untilAsserted(runnable);
+	}
 }
